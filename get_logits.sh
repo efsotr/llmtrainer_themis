c6a28034ef6a8339c4cab2872605c4cb0733e203
efsotr <efsotr_l@stu.pku.edu.cn>
2024-11-26 22:44:46 +0800

    refine example about get_ids.sh

--- get_logits.sh
+++ get_logits.sh
@@ -1,6 +1,6 @@
 export CUDA_VISIBLE_DEVICES=0,1,2,3
 export MODEL_NAME="llama-3-8b-it"
-export DATA_NAME="ori-uf.avg_fine_score.${MODEL_NAME}_ids"
+export DATA_NAME="uf.fine-grained.${MODEL_NAME}_ids"
 export PORT=30001
 
 name=get_ref
