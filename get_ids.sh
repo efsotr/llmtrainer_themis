d87323e8c6ebf7ee5bb67353b7800928f2b6b7be
efsotr <efsotr_l@stu.pku.edu.cn>
2024-11-26 22:43:54 +0800

    refine example about get_ids.sh

--- get_ids.sh
+++ get_ids.sh
@@ -1,11 +1,11 @@
 export MODEL_NAME="llama-3-8b-it"
-DATA_DIR=ori_ultrafeedback
-DATA_NAME="ori-uf.avg_fine_score"
+DATA_DIR=ultrafeedback_fine-grained
+DATA_NAME="uf.fine-grained"
 python get_tokenized_data.py \
     --model_path ./models/$MODEL_NAME \
     --prompt_type chat \
     --training_type po \
     --train_dataset_path train_data/$DATA_DIR/$DATA_NAME.train.json.gz \
-    --train_save_path ./cache/$DATA_NAME.${MODEL_NAME}_ids.train.json.gz \
+    --train_save_path ./cache_ids/$DATA_NAME.${MODEL_NAME}_ids.train.json.gz \
     --dev_dataset_path train_data/$DATA_DIR/$DATA_NAME.dev.json \
-    --dev_save_path ./cache/$DATA_NAME.${MODEL_NAME}_ids.dev.json \
+    --dev_save_path ./cache_ids/$DATA_NAME.${MODEL_NAME}_ids.dev.json \
